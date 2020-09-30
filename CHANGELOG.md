2ad611308a0e3f210c846d9898256a17d7eddcba
umang91 <umangchamaria@gmail.com>
2020-09-30 19:06:46 +0530

    packages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,11 @@
-# 1.1.0 (30-09-2020)
+### 1.1.0 (30-09-2020)
 - Added support for Push Templates 
 - Provided GDPR OptOut APIs
 - iOS Native SDK Dependency set to MoEPluginBase 1.0.0
 - Android SDK moved to `androidx` namespace
 
-# 1.0.1 (07-07-2020)
+### 1.0.1 (07-07-2020)
 - Android SDK Version update
 
-# 1.0.0 (05-07-2020)
+### 1.0.0 (05-07-2020)
 - Initial Release
---
+++ ReleaseAssets/iOS/MoEngageConfiguration.h
@@ -0,0 +1,15 @@
+
+//
+//  MoEngageConfiguration.m
+//  MoEngage
+//
+//  Created by Chengappa on 28/06/20.
+//  Copyright © 2020 MoEngage. All rights reserved.
+//
+
+#define kMoEngageAppID @"Your App ID"
+#define kMoEngageRegion @"DEFAULT"  // DEFAULT / EU / SERV3
+#define kMoEngageLogsEnabled NO
+#define kDefaultIDFATrackingOptedOut NO
+#define kDefaultIDFVTrackingOptedOut NO
+
