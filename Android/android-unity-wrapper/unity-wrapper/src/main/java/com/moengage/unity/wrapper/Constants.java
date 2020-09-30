189d62308ffc67a292f6452a46a41cad82a7b57e
umang91 <umangchamaria@gmail.com>
2020-09-30 17:53:24 +0530

    unity wrapper update, dependencies updated

--- Android/android-unity-wrapper/unity-wrapper/src/main/java/com/moengage/unity/wrapper/Constants.java
+++ Android/android-unity-wrapper/unity-wrapper/src/main/java/com/moengage/unity/wrapper/Constants.java
@@ -20,7 +20,7 @@ package com.moengage.unity.wrapper;
 interface Constants {
   String MODULE_TAG = "Unity_";
   String INTEGRATION_TYPE = "unity";
-  String INTEGRATION_VERSION = "1.0.1";
+  String INTEGRATION_VERSION = "1.1.0";
 
   String METHOD_NAME_PUSH_REDIRECTION = "PushClicked";
   String METHOD_NAME_IN_APP_SHOWN = "InAppCampaignShown";
