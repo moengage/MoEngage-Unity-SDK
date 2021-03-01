7a3906d120a4a381bb65268e946167f9f7b7ece8
umangmoe <umang@moengage.com>
2021-03-01 17:22:28 +0530

    release assets update

--- Android/android-unity-wrapper/buildSrc/src/main/kotlin/ReleaseProperties.kt
+++ Android/android-unity-wrapper/buildSrc/src/main/kotlin/ReleaseProperties.kt
@@ -26,6 +26,6 @@ object ReleaseConfig {
     const val artifactId = "unity-wrapper"
     const val artifactName = "MoEngage Unity Wrapper"
     const val description = "Wrapper SDK for unity framework support on MoEngage Platform."
-    const val versionName = "1.3.01"
-    const val versionCode = 1301
+    const val versionName = "2.0.00"
+    const val versionCode = 2000
 }
